4827f19e29a1742d46a681ec146e67375d45e3b9
jayantced739 <109576199+jayantced@users.noreply.github.com>
2023-05-17 16:28:49 +0530

    www

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 console.log('wwwwwww');
 
 window.alert("fgddfdggfd");
+let firstName = "Jayant";
+let age = "50";
+let student = false;
+
+console.log(firstName);
+console.log(age);
+console.log(student);
