f1a07eb0563e2e1632cc9b35a3761202f60043cf
jayantced739 <109576199+jayantced@users.noreply.github.com>
2023-05-18 19:00:36 +0530

    const keyword

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
-console.log('wwwwwww');
+// console.log('wwwwwww');
 
-//window.alert("fgddfdggfd");
-let firstName = "Jayant";
-let age = "50";
-let student = false;
+// //window.alert("fgddfdggfd");
+// let firstName = "Jayant";
+// let age = "50";
+// let student = false;
 
-console.log("Hello", firstName);
-console.log("You are", age, "old");
-console.log(student);
+// console.log("Hello", firstName);
+// console.log("You are", age, "old");
+// console.log(student);
 
-document.getElementById("p1").innerHTML = "Hello " + firstName;
+// document.getElementById("p1").innerHTML = "Hello " + firstName;
 
-let students = 20;
+// let students = 20;
 
-students %= 2;
+// students %= 2;
 
-console.log(students);
+// console.log(students);
 
-let userName = window.prompt("Enter zYour Name");
-console.log(userName);
+/* let userName = window.prompt("Enter Your Name");
+console.log(userName); */
 
 
-let username;
+// let username;
 
-document.getElementById("user-name-btn").onclick = function(){
-    username = document.getElementById("user-name").value;
-    console.log(username);
-}
+// document.getElementById("user-name-btn").onclick = function() {
+//     username = document.getElementById("user-name").value;
+//     console.log(username);
+// }
 
-//system 2
+// let age = Number(window.prompt("Enter your age"));
+// console.log(age);
+
+// age += 1;
+// console.log(age, typeof age);
+
+const pi = 3.14;
+let radius = Number(window.prompt("Enter radius"));
+let circumference;
+
+circumference = 2 * pi * radius;
+console.log(circumference);
