d609fe70fe77fe89fc183a6073c3eb5ae5bcdff9
jayantced739 <109576199+jayantced@users.noreply.github.com>
2023-05-18 09:13:39 +0530

    sss

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 console.log('wwwwwww');
 
-window.alert("fgddfdggfd");
+//window.alert("fgddfdggfd");
 let firstName = "Jayant";
 let age = "50";
 let student = false;
 
-console.log(firstName);
-console.log(age);
+console.log("Hello", firstName);
+console.log("You are", age, "old");
 console.log(student);
+
+document.getElementById("p1").innerHTML = "Hello " + firstName;
+
+let students = 20;
+
+students %= 2;
+
+console.log(students);
+
+let userName = window.prompt("Enter zYour Name");
+console.log(userName);
+
+
+let username;
+
+document.getElementById("user-name-btn").onclick = function(){
+    username = document.getElementById("user-name").value;
+    console.log(username);
+}
