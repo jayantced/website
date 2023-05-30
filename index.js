d586b18a59f4e96c20db57d20dc2905ddd102747
jayantced739 <109576199+jayantced@users.noreply.github.com>
2023-05-30 15:35:16 +0530

    sss

--- index.js
+++ index.js
@@ -34,9 +34,24 @@ console.log(userName); */
 // age += 1;
 // console.log(age, typeof age);
 
-const pi = 3.14;
-let radius = Number(window.prompt("Enter radius"));
-let circumference;
+// const pi = 3.14;
+// let radius = Number(window.prompt("Enter radius"));
+// let circumference;
 
-circumference = 2 * pi * radius;
-console.log(circumference);
+// circumference = 2 * pi * radius;
+// console.log(circumference);
+
+let height;
+let base;
+let hypotenuse;
+
+
+// hypotenuse = Math.hypot(3, 4);
+
+document.getElementById("tri-submit").onclick = function() {
+    height = document.getElementById("height").value;
+    base = document.getElementById("base").value;
+    hypotenuse = Math.sqrt( Number(Math.pow(height, 2)) + Number(Math.pow(base, 2)) );
+    document.getElementById("hypotenuse").innerHTML = "hypotenuse is " + hypotenuse;
+    // console.log(typeof(Math.pow(height, 2)));
+}
